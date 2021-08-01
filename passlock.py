86a157936d98b1eb0544fc46b2090843a609ec69
franklin <franklinwanami@gmail.com>
2021-08-01 23:24:49 +0300

    create User class that defines,stores each user instance

---
+++ passlock.py
@@ -0,0 +1,15 @@
+class Passlock:
+
+    '''
+    Class that generates new instances of user.
+    '''
+
+    user = [] # Empty user list
+
+    def __init__(self, username, password, email):
+
+        # docstring removed for simplicity
+
+        self.username = username
+        self.password = password
+        self.email = email
